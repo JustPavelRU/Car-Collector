9579d13dcba5eaf1b510279c2f6fc1d58ddfd30e
JustPavelRU <petroove.pavel@yandex.ru>
2023-12-14 17:24:23 +0300

    Initial commit

---
+++ src/test/java/com/app/carcollector/CarCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.carcollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
